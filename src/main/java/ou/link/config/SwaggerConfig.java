82a441c657f70f7a72e6132dc50ea3d0a8013da3
maxjdev <maxjramosdev@gmail.com>
2025-02-16 11:06:52 -0300

    add swagger configs

---
+++ src/main/java/ou/link/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package ou.link.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("URL Shortener")
+                        .description("URL shortener for full stack application")
+                        .license(new License().name("MIT License").url("https://opensource.org/licenses/MIT"))
+                        .contact(new Contact().name("maxjdev").email("maxjramosdev@gmail.com").url("https://www.linkedin.com/in/maxjdev/")));
+    }
+}
